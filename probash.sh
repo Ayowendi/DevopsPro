01d5755e863e3f1929c101a30ab539e6f1fe9e7f
Ayo Olurebi <100032633+Ayowendi@users.noreply.github.com>
2022-09-01 15:18:48 -0500

    Update probash.sh
    commented out what isnt needed

--- probash.sh
+++ probash.sh
@@ -2,10 +2,10 @@
 
 sudo apt-get update 
 sudo apt-get install -y docker.io
-sudo apt-get install -y default-jre
-sudo apt-get install -y default-jdk
-sudo apt install -y jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
-sudo usermod -aG docker jenkins
+#sudo apt-get install -y default-jre
+#sudo apt-get install -y default-jdk
+#sudo apt install -y jenkins
+#sudo systemctl start jenkins
+#sudo systemctl status jenkins
+# usermod -aG docker jenkins
 
